ca87b2b270dd5aee0f554a06da0aee1f5da70c52
NucaToasTer <ethan.a.quandt@gmail.com>
2026-03-10 10:07:21 +0100

    cleanup

--- textbox label/boilerplate/Form1.Designer.cs
+++ textbox label/boilerplate/Form1.Designer.cs
@@ -2,33 +2,13 @@
 
 partial class Form1
 {
-    /// <summary>
-    ///  Required designer variable.
-    /// </summary>
     private System.ComponentModel.IContainer components = null;
     private System.Windows.Forms.TextBox textBox1;
     private System.Windows.Forms.TextBox textBox2;
     private System.Windows.Forms.Label label1;
-
-    /// <summary>
-    ///  Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-    protected override void Dispose(bool disposing)
-    {
-        if (disposing && (components != null))
-        {
-            components.Dispose();
-        }
-        base.Dispose(disposing);
-    }
-
-    #region Windows Form Designer generated code
-
-    /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
-    /// </summary>
+    
+    #region Windows Form Designer generated code;
+    
     private void InitializeComponent()
     {
 
@@ -62,4 +42,5 @@ partial class Form1
     }
 
     #endregion
+
 }
