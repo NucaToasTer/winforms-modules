1a3f552db9b0e18613a73307900fb05d2dda295b
NucaToasTer <ethan.a.quandt@gmail.com>
2026-03-16 18:21:53 +0100

    Update Form1.cs

--- groupbox/placeholder/Form1.cs
+++ groupbox/placeholder/Form1.cs
@@ -6,13 +6,12 @@ public partial class Form1 : Form
     {
         InitializeComponent();
         
-        // Optional: Add some controls to the GroupBox programmatically
         AddControlsToGroupBox();
     }
     
     private void AddControlsToGroupBox()
     {
-        // Example: Add a label to the group box
+
         Label label1 = new Label
         {
             Text = "This is inside the GroupBox",
@@ -20,7 +19,6 @@ public partial class Form1 : Form
             AutoSize = true
         };
         
-        // Example: Add a button to the group box
         Button button1 = new Button
         {
             Text = "Click Me",
@@ -28,7 +26,6 @@ public partial class Form1 : Form
             Size = new System.Drawing.Size(100, 30)
         };
         
-        // Add the controls to the group box
         groupBox1.Controls.Add(label1);
         groupBox1.Controls.Add(button1);
     }
