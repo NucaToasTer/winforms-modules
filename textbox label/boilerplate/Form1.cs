f83112c7bf6f859f2cd6a0c4bda64861892a9662
NucaToasTer <ethan.a.quandt@gmail.com>
2026-03-16 18:25:37 +0100

    Update Form1.cs

--- textbox label/boilerplate/Form1.cs
+++ textbox label/boilerplate/Form1.cs
@@ -18,12 +18,11 @@ public partial class Form1 : Form
 
             Console.WriteLine("User entered: " + message);
             
-            // Update label with the entered text
             label1.Text = $"Last input from TextBox 1: {message}";
 
             textBox1.Clear();
 
-            e.SuppressKeyPress = true; // prevents ding sound
+            e.SuppressKeyPress = true;
         }
     }
 
@@ -35,12 +34,11 @@ public partial class Form1 : Form
 
             Console.WriteLine("User 2 entered: " + message);
             
-            // Update label with the entered text
             label1.Text = $"Last input from TextBox 2: {message}";
 
             textBox2.Clear();
 
-            e.SuppressKeyPress = true; // prevents ding sound
+            e.SuppressKeyPress = true;
         }
     }
 }
