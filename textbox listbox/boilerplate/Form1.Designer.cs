8cda1593e1cffcb9e224442f0917d1637d2a5e2a
NucaToasTer <ethan.a.quandt@gmail.com>
2026-03-16 18:26:55 +0100

    Update Form1.Designer.cs

--- textbox listbox/boilerplate/Form1.Designer.cs
+++ textbox listbox/boilerplate/Form1.Designer.cs
@@ -2,18 +2,12 @@
 
 partial class Form1
 {
-    /// <summary>
-    ///  Required designer variable.
-    /// </summary>
+
     private System.ComponentModel.IContainer components = null;
     private System.Windows.Forms.TextBox textBox1;
     private System.Windows.Forms.TextBox textBox2;
     private System.Windows.Forms.ListBox listBox1;
 
-    /// <summary>
-    ///  Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
     protected override void Dispose(bool disposing)
     {
         if (disposing && (components != null))
@@ -25,10 +19,6 @@ partial class Form1
 
     #region Windows Form Designer generated code
 
-    /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
-    /// </summary>
     private void InitializeComponent()
     {
         this.components = new System.ComponentModel.Container();
@@ -36,26 +26,22 @@ partial class Form1
         this.ClientSize = new System.Drawing.Size(800, 450);
         this.Text = "Form1";
         
-        // TextBox1
         this.textBox1 = new System.Windows.Forms.TextBox();
         this.textBox1.Location = new System.Drawing.Point(20, 20);
         this.textBox1.Size = new System.Drawing.Size(300, 23);
         this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
         this.Controls.Add(this.textBox1);
         
-        // TextBox2
         this.textBox2 = new System.Windows.Forms.TextBox();
         this.textBox2.Location = new System.Drawing.Point(20, 60);
         this.textBox2.Size = new System.Drawing.Size(300, 23);
         this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
         this.Controls.Add(this.textBox2);
         
-        // ListBox1
         this.listBox1 = new System.Windows.Forms.ListBox();
         this.listBox1.Location = new System.Drawing.Point(20, 100);
         this.listBox1.Size = new System.Drawing.Size(300, 100);
         
-        // Add items to the ListBox
         this.listBox1.Items.AddRange(new object[] {
             "Item 1",
             "Item 2"
