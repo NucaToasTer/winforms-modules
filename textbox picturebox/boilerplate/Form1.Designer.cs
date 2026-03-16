b964544f924d58db7d281a7b59a98dae0c1be5bb
NucaToasTer <ethan.a.quandt@gmail.com>
2026-03-16 18:28:21 +0100

    Update Form1.Designer.cs

--- textbox picturebox/boilerplate/Form1.Designer.cs
+++ textbox picturebox/boilerplate/Form1.Designer.cs
@@ -2,18 +2,12 @@
 
 partial class Form1
 {
-    /// <summary>
-    ///  Required designer variable.
-    /// </summary>
+
     private System.ComponentModel.IContainer components = null;
     private System.Windows.Forms.TextBox textBox1;
     private System.Windows.Forms.TextBox textBox2;
     private System.Windows.Forms.PictureBox pictureBox1;
 
-    /// <summary>
-    ///  Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
     protected override void Dispose(bool disposing)
     {
         if (disposing && (components != null))
@@ -25,10 +19,6 @@ partial class Form1
 
     #region Windows Form Designer generated code
 
-    /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
-    /// </summary>
     private void InitializeComponent()
     {
         this.components = new System.ComponentModel.Container();
