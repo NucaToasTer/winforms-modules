f6f3564ccaffd5daee51ed6994f84b68cb793c4d
NucaToasTer <ethan.a.quandt@gmail.com>
2026-03-16 18:23:41 +0100

    Update Form1.Designer.cs

--- progress bar/placeholder/Form1.Designer.cs
+++ progress bar/placeholder/Form1.Designer.cs
@@ -2,15 +2,9 @@
 
 partial class Form1
 {
-    /// <summary>
-    ///  Required designer variable.
-    /// </summary>
+
     private System.ComponentModel.IContainer components = null;
 
-    /// <summary>
-    ///  Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
     protected override void Dispose(bool disposing)
     {
         if (disposing && (components != null))
@@ -22,24 +16,18 @@ partial class Form1
 
     #region Windows Form Designer generated code
 
-    /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
-    /// </summary>
     private void InitializeComponent()
     {
         this.components = new System.ComponentModel.Container();
         this.progressBar1 = new System.Windows.Forms.ProgressBar();
         this.SuspendLayout();
         
-        // progressBar1
         this.progressBar1.Location = new System.Drawing.Point(50, 200);
         this.progressBar1.Name = "progressBar1";
         this.progressBar1.Size = new System.Drawing.Size(700, 30);
         this.progressBar1.TabIndex = 0;
-        this.progressBar1.Value = 50; // Set initial value to 50%
+        this.progressBar1.Value = 50;
 
-        // Form1
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         this.ClientSize = new System.Drawing.Size(800, 450);
         this.Controls.Add(this.progressBar1);
